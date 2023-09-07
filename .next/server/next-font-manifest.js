8003e84a9c1e0f0a570e5811b34423e82e59e3a3
Nuwan <nuwan@192.168.1.18>
2023-09-07 16:06:14 +0530

    Init 3

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/nuwan/Documents/CYB_POS_V1/cyb_pos_backend_fixed/src/app/layout\":[\"static/media/2aaf0723e720e8b9-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
